341a09f47c2cc3dfa093ea22295f07b3fb578faa
Nivetha R <Nivetha.R@splus.local>
2023-08-22 11:06:49 +0530

    update dataSearchFilterController

--- controllers/dataSearchFilter.controller.js
+++ controllers/dataSearchFilter.controller.js
@@ -13,116 +13,106 @@ require("dotenv").config();
 
 
 exports.getSearchFilterData = async(req, res) => {
-  try {  
-   
-      const religions = req.body.religion;
-      const scripts = req.body.script;
-      const searchText = req.body.searchText;
-      const filteredUsers = await surnamesModel.aggregate([
-        {
-          $search: {
-            index: "fuzzy3",
-            compound: {
-              should: [
-                {
-                  autocomplete: {
-                    query: searchText, // Use the search text from the request
-                    path: "community",
-                  },
-                },
-                {
-                  autocomplete: {
-                    query: searchText, // Use the search text from the request
-                    path: "surname",
-                  },
-                },
-                {
-                  autocomplete: {
-                    query: searchText, // Use the search text from the request
-                    path: "meaning",
-                  },
-                },
-              ],
-            },
-          },
-        },
-        {
-          $match: {
-            religion: { $in: religions },
-            script: { $in: scripts },
-            // Additional match conditions if needed
-          },
-        },
-        {
-          $facet: {
-            religionFacet: [
-              {
-                $group: {
-                  _id: "$religion",
-                  count: { $sum: 1 },
-                  artists: {
-                    $push: {
-                      name: "$surname",
-		                  religion: "$religion",
-                      script: "$script",
-		                  community: "$community",
-		                  kuldevtaFamilyDeity: "$kuldevtaFamilyDeity",
-                      alternative: "$alternative",
-		                  createdBy: "$createdBy",
-		                  meaning: "$meaning",
-		                  sStatus: "$sStatus",
-		                  vansha: "$vansha",
-		                  veda: "$veda",
-		                  history: "$history",
-		                  wikiUrl: "$wikiUrl",
-		                  translations: "$translations",
-		                  createdAt: "$createdAt",
-		                  updatedAt: "$updatedAt"
-                    }
-                  }
-                }
-              }
-            ],
-            scriptFacet: [
-              {
-                $group: {
-                  _id: "$script",
-                  count: { $sum: 1 },
-                  artists: {
-                    $push: {
-                      name: "$surname",
-		                  religion: "$religion",
-                      script: "$script",
-		                  community: "$community",
-		                  kuldevtaFamilyDeity: "$kuldevtaFamilyDeity",
-                      alternative: "$alternative",
-		                  createdBy: "$createdBy",
-		                  meaning: "$meaning",
-		                  sStatus: "$sStatus",
-		                  vansha: "$vansha",
-		                  veda: "$veda",
-		                  history: "$history",
-		                  wikiUrl: "$wikiUrl",
-		                  translations: "$translations",
-		                  createdAt: "$createdAt",
-		                  updatedAt: "$updatedAt"
-                    }
-                  }
-                }
-              }
-            ],
-      
-      
-          }
-        }
-      ])
-      // const data = await surnamesModel.find({}).limit(1)
-      console.log(filteredUsers)
-      res.status(200).send(filteredUsers)
+  try {
 
-      // res.send(data);
-   } catch (e) {
-    console.log(e)
-        res.status(400).send(e)
+    const religions = req.body.religion;
+    const scripts = req.body.script;
+    const searchText = req.body.searchText;
+
+     
+    const matchConditions = {}
+    if (religions && religions.length>0) {
+      matchConditions.religion = { $in: religions };
     }
+    if (scripts && scripts.length>0) {
+      matchConditions.script = { $in: scripts};
+    }
+
+    const searchQuery = searchText ? searchText.trim() : "";
+
+    
+
+    // If searchQuery has content, add it to the $search compound query
+    
+
+    const filteredUsers = await surnamesModel.aggregate([
+      {
+        
+        $match: {
+          $or: [
+            { community: { $regex: searchQuery, $options: "i" } },
+            { surname: { $regex: searchQuery, $options: "i" } },
+            { meaning: { $regex: searchQuery, $options: "i" } },
+          ],
+          ...matchConditions,
+        },
+      },
+      {
+        $facet: {
+          religionFacet: [
+            {
+              $group: {
+                _id: "$religion",
+                count: { $sum: 1 },
+                artists: {
+                  $push: {
+                    name: "$surname",
+                    religion: "$religion",
+                    script: "$script",
+                    community: "$community",
+                    kuldevtaFamilyDeity: "$kuldevtaFamilyDeity",
+                    alternative: "$alternative",
+                    createdBy: "$createdBy",
+                    meaning: "$meaning",
+                    sStatus: "$sStatus",
+                    vansha: "$vansha",
+                    veda: "$veda",
+                    history: "$history",
+                    wikiUrl: "$wikiUrl",
+                    translations: "$translations",
+                    createdAt: "$createdAt",
+                    updatedAt: "$updatedAt"
+                  }
+                }
+              }
+            }
+          ],
+          scriptFacet: [
+            {
+              $group: {
+                _id: "$script",
+                count: { $sum: 1 },
+                artists: {
+                  $push: {
+                    name: "$surname",
+                    religion: "$religion",
+                    script: "$script",
+                    community: "$community",
+                    kuldevtaFamilyDeity: "$kuldevtaFamilyDeity",
+                    alternative: "$alternative",
+                    createdBy: "$createdBy",
+                    meaning: "$meaning",
+                    sStatus: "$sStatus",
+                    vansha: "$vansha",
+                    veda: "$veda",
+                    history: "$history",
+                    wikiUrl: "$wikiUrl",
+                    translations: "$translations",
+                    createdAt: "$createdAt",
+                    updatedAt: "$updatedAt"
+                  }
+                }
+              }
+            }
+          ],
+
+
+      }
+      }
+    ])
+    res.status(200).send(filteredUsers)
+  } catch (e) {
+    console.log(e)
+    res.status(400).send(e)
+  }
 }
